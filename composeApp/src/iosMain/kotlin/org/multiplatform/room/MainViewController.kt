7fe270495bdb752c396e46e552afcee03ff129f6
fluttertutorialin <kamal.lakhani56@gmail.com>
2024-11-10 10:02:37 +0530

    initial

--- composeApp/src/iosMain/kotlin/org/multiplatform/room/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/multiplatform/room/MainViewController.kt
@@ -1,7 +1,9 @@
 package org.multiplatform.room
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.multiplatform.room.di.initKoin
 
 fun MainViewController() = ComposeUIViewController {
+    initKoin()
     App()
 }
